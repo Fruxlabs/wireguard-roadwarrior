c5b4341ba8e26fdb68d93f452610cd6d06cd3cd3
Eshan Aswar <>
2018-10-19 00:23:30 +0530

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -158,13 +158,6 @@ echo "You can leave the default options and just press enter if you are ok with 
 echo
 echo "First, Provide the IPv4 address of the network interface you want Wireguard"
 echo "listening to."
-# Autodetect IP address and pre-fill for the user
-IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-read -p "IP address: " -e -i $IP IP
-# If $IP is a private IP address, the server must be behind NAT
-if echo "$IP" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
-echo
-echo "This server is behind NAT. What is the public IPv4 address or hostname?"
 read -p "Public IP address / hostname: " -e PUBLICIP
 fi
 echo
